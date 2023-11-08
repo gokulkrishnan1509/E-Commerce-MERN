71486c9593aea866af09c1497aa19ba4b42f1614
gokulkrishnan1509 <gokulkrishnan1509@gmail.com>
2023-11-08 11:37:11 +0530

    backend

--- E-backend/utils/refreshToken.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.SECERET_STRING, { expiresIn: "3d" });
-};
-
-module.exports = { generateRefreshToken };
